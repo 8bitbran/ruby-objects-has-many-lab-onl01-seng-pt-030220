82175550081092e664980351dcd078082fc615c8
Brandon Marrero <brandonmar97@gmail.com>
2020-03-26 22:54:53 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Post 
+  attr_accessor :title, :author, :name
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    new
+  end
+  
+  def new 
+    @@all << self
+  end
+  
+  def author_name
+    self.author.name 
+  end 
+  
+  def self.all 
+    @@all
+  end 
+end 
