bcc7973359447dab3987fec64ad85e1c91c4b1f3
Brandon Marrero <brandonmar97@gmail.com>
2020-03-26 23:08:48 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -15,7 +15,11 @@ class Post
   end
   
   def author_name
-    self.author.name || 
+    if self.author.name 
+      return self.author.name 
+    else
+      return nil 
+    end 
   end 
   
   def self.all 
